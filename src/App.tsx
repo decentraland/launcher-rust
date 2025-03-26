097db674deaa795859e5a032366ba538a9e4087b
NickKhalow <nickhalow@gmail.com>
2025-03-26 15:19:01 +0200

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import { Container, CssBaseline } from 'decentraland-ui2';
-import { dark } from 'decentraland-ui2/dist/theme';
+import { Container } from 'decentraland-ui2';
+import { dark, ThemeProvider } from 'decentraland-ui2/dist/theme'
 import { Home } from './components/Home/Home';
 
 export const App: React.FC = () => {
   return (
     <React.StrictMode>
       <ThemeProvider theme={dark}>
-        <CssBaseline />
         <Container id="app-container" fixed sx={{ display: 'flex', height: '100vh' }}>
           <Home />
         </Container>
