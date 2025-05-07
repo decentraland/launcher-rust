6c33f1bb448d018a0428857b6a24ff45a2fa9404
NickKhalow <nickhalow@gmail.com>
2025-05-07 17:04:58 +0300

    fix codestyle

--- src/components/Home/Home.styles.ts
+++ src/components/Home/Home.styles.ts
@@ -1,59 +1,58 @@
-import { styled, LinearProgress, Button } from 'decentraland-ui2';
+import { styled, LinearProgress, Button } from "decentraland-ui2";
 
-export const Landscape = styled('div')(_props => ({
-  position: 'absolute',
+export const Landscape = styled("div")((_props) => ({
+  position: "absolute",
   top: 0,
   left: 0,
   bottom: 0,
-  width: '100%',
-  height: '100%',
-  overflow: 'hidden',
+  width: "100%",
+  height: "100%",
+  overflow: "hidden",
   zIndex: -1,
-  '::after': {
-    position: 'absolute',
+  "::after": {
+    position: "absolute",
     top: 0,
     left: 0,
     bottom: 0,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     content: "''",
-    mixBlendMode: 'multiply',
-    pointerEvents: 'none',
+    mixBlendMode: "multiply",
+    pointerEvents: "none",
   },
   img: {
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover',
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
   },
 }));
 
-export const Logo = styled('img')(props => ({
+export const Logo = styled("img")((props) => ({
   ...props,
-  height: '61px',
-  width: '61px',
+  height: "61px",
+  width: "61px",
 }));
 
-export const ErrorIcon = styled('img')(props => ({
+export const ErrorIcon = styled("img")((props) => ({
   ...props,
-  height: '62px',
-  width: '62px',
+  height: "62px",
+  width: "62px",
 }));
 
-export const ErrorDialogButton = styled(Button)(props => ({
+export const ErrorDialogButton = styled(Button)((props) => ({
   ...props,
-  width: '190px',
-  height: '46px',
+  width: "190px",
+  height: "46px",
 }));
 
-
-export const LoadingBar = styled(LinearProgress)(props => ({
+export const LoadingBar = styled(LinearProgress)((props) => ({
   ...props,
-  width: '348px',
-  height: '7px',
-  backgroundColor: 'rgba(255, 255, 255, 0.1)',
-  borderRadius: '3.5px',
-  '& .MuiLinearProgress-bar': {
-    background: 'linear-gradient(90deg, #FF2D55 0%, #FFBC5B 100%)',
-    borderRadius: '3.5px'
-  }
+  width: "348px",
+  height: "7px",
+  backgroundColor: "rgba(255, 255, 255, 0.1)",
+  borderRadius: "3.5px",
+  "& .MuiLinearProgress-bar": {
+    background: "linear-gradient(90deg, #FF2D55 0%, #FFBC5B 100%)",
+    borderRadius: "3.5px",
+  },
 }));
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,39 +1,45 @@
-import React, { memo, useEffect, useState } from 'react';
-import { Box, Typography } from 'decentraland-ui2';
-import { Status, BuildType } from './types';
-import { Landscape, LoadingBar, Logo, ErrorIcon, ErrorDialogButton } from './Home.styles';
+import React, { memo, useEffect, useState } from "react";
+import { Box, Typography } from "decentraland-ui2";
+import { Status, BuildType } from "./types";
+import {
+  Landscape,
+  LoadingBar,
+  Logo,
+  ErrorIcon,
+  ErrorDialogButton,
+} from "./Home.styles";
 
-import LANDSCAPE_IMG from '../../assets/background.jpg';
-import LOGO_SVG from '../../assets/logo.svg';
-import ERROR_SVG from '../../assets/error.svg';
+import LANDSCAPE_IMG from "../../assets/background.jpg";
+import LOGO_SVG from "../../assets/logo.svg";
+import ERROR_SVG from "../../assets/error.svg";
 
-import { invoke, Channel } from '@tauri-apps/api/core';
-import { LogicalSize, getCurrentWindow } from '@tauri-apps/api/window';
-import { exit } from '@tauri-apps/plugin-process';
+import { invoke, Channel } from "@tauri-apps/api/core";
+import { LogicalSize, getCurrentWindow } from "@tauri-apps/api/window";
+import { exit } from "@tauri-apps/plugin-process";
 
 type WindowSize = {
   width: number;
   height: number;
-}
+};
 
 function asLogicalSize(size: WindowSize) {
   return new LogicalSize(size.width, size.height);
 }
 
 const stateWindowSize = {
-  "width": 600,
-  "height": 156
-}
+  width: 600,
+  height: 156,
+};
 
 const errorWindowSize = {
-  "width": 600,
-  "height": 358
-}
+  width: 600,
+  height: 358,
+};
 
 const resizeWindow = async (size: WindowSize) => {
   const logicalSize = asLogicalSize(size);
   await getCurrentWindow().setSize(logicalSize).catch(console.error);
-}
+};
 
 const useChannelUpdates = (channel: Channel<Status>) => {
   const [currentStatus, setCurrentStatus] = useState<Status | null>(null);
@@ -49,12 +55,11 @@ const useChannelUpdates = (channel: Channel<Status>) => {
 };
 
 export const Home: React.FC = memo(() => {
-
   const [channel] = useState(new Channel<Status>());
   const currentStatus = useChannelUpdates(channel);
 
   const launchFlow = async () => {
-    await invoke('launch', { channel }).catch(console.error);
+    await invoke("launch", { channel }).catch(console.error);
   };
 
   useEffect(() => {
@@ -65,42 +70,44 @@ export const Home: React.FC = memo(() => {
     if (!currentStatus) return null;
 
     switch (currentStatus.event) {
-      case 'state':
+      case "state":
         switch (currentStatus.data.step.event) {
-          case 'fetching':
+          case "fetching":
             return renderFetchStep();
-          case 'downloading':
-            {
-              let data = currentStatus.data.step.data;
-              let isUpdate = data.buildType === BuildType.Update;
-              let progress = data.progress;
-              return renderDownloadStep(isUpdate, progress);
-            }
-          case 'installing':
+          case "downloading": {
+            let data = currentStatus.data.step.data;
+            let isUpdate = data.buildType === BuildType.Update;
+            let progress = data.progress;
+            return renderDownloadStep(isUpdate, progress);
+          }
+          case "installing":
             let data = currentStatus.data.step.data;
             let isUpdate = data.buildType === BuildType.Update;
             return renderInstallStep(isUpdate);
-          case 'launching':
+          case "launching":
             return renderLaunchStep();
         }
-      case 'error':
+      case "error":
         return renderError(currentStatus.data.message);
       default:
         return null;
     }
   };
 
-  const renderFetchStep = () =>
-    renderStep('Fetching Latest...')
+  const renderFetchStep = () => renderStep("Fetching Latest...");
 
   const renderDownloadStep = (isUpdate: boolean, downloadingProgress: number) =>
-    renderStep(isUpdate ? 'Downloading Update...' : 'Downloading Decentraland...', downloadingProgress);
+    renderStep(
+      isUpdate ? "Downloading Update..." : "Downloading Decentraland...",
+      downloadingProgress,
+    );
 
   const renderInstallStep = (isUpdate: boolean) =>
-    renderStep(isUpdate ? 'Installing Update...' : 'Installation in Progress...');
+    renderStep(
+      isUpdate ? "Installing Update..." : "Installation in Progress...",
+    );
 
-  const renderLaunchStep = () =>
-    renderStep('Launching Decentraland...');
+  const renderLaunchStep = () => renderStep("Launching Decentraland...");
 
   const renderError = (message: string) => {
     resizeWindow(errorWindowSize);
@@ -110,7 +117,7 @@ export const Home: React.FC = memo(() => {
         <Typography
           variant="h5"
           sx={{
-            fontFamily: 'Inter, sans-serif',
+            fontFamily: "Inter, sans-serif",
             fontWeight: 700,
           }}
         >
@@ -119,7 +126,7 @@ export const Home: React.FC = memo(() => {
         <Typography
           variant="h6"
           sx={{
-            fontFamily: 'Inter, sans-serif',
+            fontFamily: "Inter, sans-serif",
           }}
         >
           {message}
@@ -134,10 +141,7 @@ export const Home: React.FC = memo(() => {
           >
             EXIT
           </ErrorDialogButton>
-          <ErrorDialogButton
-            variant="contained"
-            onClick={launchFlow}
-          >
+          <ErrorDialogButton variant="contained" onClick={launchFlow}>
             RETRY
           </ErrorDialogButton>
         </Box>
@@ -145,27 +149,53 @@ export const Home: React.FC = memo(() => {
     );
   };
 
-  const renderStep = (message: string, downloadingProgress: number | undefined = undefined) => {
+  const renderStep = (
+    message: string,
+    downloadingProgress: number | undefined = undefined,
+  ) => {
     resizeWindow(stateWindowSize);
     return (
       <>
         <Logo src={LOGO_SVG} />
-        <Box display="flex" flexDirection="column" justifyContent="space-between" height="61px">
-          <Typography variant="h6" align="left"
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          height="61px"
+        >
+          <Typography
+            variant="h6"
+            align="left"
             sx={{
-              fontFamily: 'Inter, sans-serif',
+              fontFamily: "Inter, sans-serif",
               fontWeight: 700,
-              fontSize: '20px',
-              lineHeight: '160%',
-              letterSpacing: '0px',
-              verticalAlign: 'middle'
+              fontSize: "20px",
+              lineHeight: "160%",
+              letterSpacing: "0px",
+              verticalAlign: "middle",
             }}
           >
             {message}
           </Typography>
-          <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-            <LoadingBar variant={downloadingProgress ? "determinate" : undefined} value={downloadingProgress ?? undefined} sx={{ mr: 1 }} />
-            {<Typography variant="body1" width="45px" visibility={downloadingProgress ? "visible" : "hidden"}>{`${Math.round(downloadingProgress ?? 0)}%`}</Typography>}
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <LoadingBar
+              variant={downloadingProgress ? "determinate" : undefined}
+              value={downloadingProgress ?? undefined}
+              sx={{ mr: 1 }}
+            />
+            {
+              <Typography
+                variant="body1"
+                width="45px"
+                visibility={downloadingProgress ? "visible" : "hidden"}
+              >{`${Math.round(downloadingProgress ?? 0)}%`}</Typography>
+            }
           </Box>
         </Box>
       </>
@@ -173,7 +203,13 @@ export const Home: React.FC = memo(() => {
   };
 
   return (
-    <Box display="flex" alignItems={'center'} justifyContent={'center'} width={'100%'} gap={4}>
+    <Box
+      display="flex"
+      alignItems={"center"}
+      justifyContent={"center"}
+      width={"100%"}
+      gap={4}
+    >
       <Landscape>
         <img src={LANDSCAPE_IMG} />
       </Landscape>
