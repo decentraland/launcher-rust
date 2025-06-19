a5f1b4c6297116d93cd1ed82f6ec2d6e1ff39887
nickkhalow <nickhalow@gmail.com>
2025-06-19 13:09:30 +0300

    version label

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -8,6 +8,7 @@ import {
   ErrorIcon,
   ErrorDialogButton,
 } from "./Home.styles";
+import { versionLabel } from "./VersionLabel";
 
 import LANDSCAPE_IMG from "../../assets/background.jpg";
 import LOGO_SVG from "../../assets/logo.svg";
@@ -238,6 +239,7 @@ export const Home: React.FC = memo(() => {
         <img src={LANDSCAPE_IMG} />
       </Landscape>
       {renderStatusMessage()}
+      {versionLabel()}
     </Box>
   );
 });
