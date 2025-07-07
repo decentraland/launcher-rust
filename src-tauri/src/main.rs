2bb11ec2a54925dc90d803f9041dbf32c68604e8
NickKhalow <nickhalow@gmail.com>
2025-07-07 15:12:28 +0300

    update clippy rules

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,3 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
-#![deny(
-    clippy::unwrap_used,
-    clippy::expect_used,
-    clippy::panic,
-    clippy::indexing_slicing,
-    clippy::arithmetic_side_effects,
-    clippy::todo,
-    clippy::dbg_macro
-)]
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
