7d23a64f9968df43013350eae0c5e5271a3533c5
Nicolas Lorusso <nalorusso@gmail.com>
2026-07-07 12:06:06 -0300

    add missing func

--- core/src/flow.rs
+++ core/src/flow.rs
@@ -108,6 +108,20 @@ impl LaunchFlow {
         }
     }
 
+    pub async fn is_deeplink_passthrough(&self) -> bool {
+        let Some(deeplink) = Protocol::value() else {
+            return false;
+        };
+
+        match self.app_launch_step.should_use_deeplink_bridge_for(&deeplink).await {
+            std::result::Result::Ok(should_use_bridge) => should_use_bridge,
+            Err(error) => {
+                log::warn!("Cannot determine deeplink passthrough state: {error}");
+                false
+            }
+        }
+    }
+
     pub async fn launch<T: EventChannel>(
         &self,
         channel: &T,
